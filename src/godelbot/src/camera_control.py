f0bd161f354bbaddd1ebe774a6c4550e7d5c9969
James Grantham <james.grantham@gmail.com>
2017-06-13 01:53:49 -0700

    cleaned up camera control and prepared to add camera_start service

--- src/godelbot/src/camera_control.py
+++ src/godelbot/src/camera_control.py
@@ -4,33 +4,98 @@ from picamera import PiCamera
 import time
 import cv2
 from datetime import datetime
+from imutils.video import FPS
 
-camera = PiCamera()
-rawCapture = PiRGBArray(camera)
+import rospy
 
-camera.resolution = (640,480)
+from threading import Thread
 
-cv2.namedWindow('Image',cv2.WINDOW_NORMAL)
-#cv2.resizeWindow('Image',600,600)
+# ----------------------------------
+# Threader Pi Camera Recording Class
+# credit to www.pyimagesearch.com 
+# ----------------------------------
+class godelbotVideoStream:
+    def __init__(self, resolution=(320, 240), framerate=15):
+        #initalize the camera and stream
+        self.camera = PiCamera()
+        self.camera.resolution = resolution
+        self.camera.framerate = framerate
+        self.rawCapture = PiRGBArray(self.camera, size=resolution)
+        self.stream = self.camera.capture_continuous(self.rawCapture, format="bgr", use_video_port=True)
 
-# allow camera to warm up
-time.sleep(1.0)
+        self.frame = None
+        self.stopped = False
+        self.camera.stop_preview()        
 
-# grab an image
-running = True
-while (running):
-    camera.capture(rawCapture, format="bgr")
-    image = rawCapture.array
-   
+    def start(self):
+        # starts the thread to read fromes from the video stream
+        Thread(target=self.update, args=()).start()
+        return self
+
+    def update(self):
+        # keep looping until thread is stopped
+        for vframe in self.stream:
+            # grab the frame
+            self.frame = vframe.array
+
+            # clear the stream for the next fram
+            self.rawCapture.truncate(0)
+
+            # stop thread if the stop flag is set
+            if self.stopped:
+                self.stream.close()
+                self.rawCapture.close()
+                self.camera.close()
+                return
+
+    def read(self):
+        # read most recent frame
+        return self.frame
+
+    def stop(self):
+        # set the stop flag
+        self.stopped = True
+
+        
+def saveFrame(godelStream, fps):
+    image = godelStream.read()     
     curtime = datetime.now().strftime("%Y-%m-%d-%H-%M-%S.%f") 
-    print curtime
     cv2.imwrite("images/" + curtime + ".jpg",image)
     cv2.imshow("Image", image)
-    
-    # empty array between captures to avoid error
-    rawCapture.truncate(0)
-    k = cv2.waitKey(100) # wait 1ms
-    if k == 27: # esc key i guess
-        cv2.destroyAllWindows()
-        running = False
-        break
+    fps.update()
+
+def captureTraining():
+    godelStream = godelbotVideoStream().start()
+
+    cv2.namedWindow('Image',cv2.WINDOW_NORMAL)
+    #cv2.resizeWindow('Image',600,600)
+
+
+        # allow camera to warm up
+    time.sleep(1.0)
+    fps = FPS().start()
+
+
+    # grab an image
+    running = True
+    while (running):
+        t = Thread(target=saveFrame(godelStream, fps))
+        # try to approximate saving at 10 FPS
+        time.sleep(0.065)
+        k = cv2.waitKey(1) # wait 1ms
+        if k == 27: # esc key i guess
+            cv2.destroyAllWindows()
+            running = False
+            fps.stop()
+            godelStream.stop()
+            print("elapsed time: {:.2f}".format(fps.elapsed()))
+            print("approx FPS: {:.2f}".format(fps.fps()))
+            break
+
+
+
+def main():
+    captureTraining()
+
+if __name__ == "__main__":
+    main()
